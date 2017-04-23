3672b28379f40ab8b6161be3016767110dd2f7c4
Christian Bühler <christian.buehler@flightright.de>
2017-04-23 17:42:24 +0200

    events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from "@angular/platform-browser";
+import {ErrorHandler, NgModule} from "@angular/core";
+import {IonicApp, IonicErrorHandler, IonicModule} from "ionic-angular";
+import {SplashScreen} from "@ionic-native/splash-screen";
+import {StatusBar} from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from "./app.component";
+import {HomePage} from "../pages/home/home";
+import {HttpModule} from "@angular/http";
+import {GooglePlus} from "@ionic-native/google-plus";
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,6 +27,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    GooglePlus,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+import {Headers, Http, URLSearchParams} from "@angular/http";
+import {GooglePlus} from "@ionic-native/google-plus";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  events: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private googlePlus: GooglePlus, private http: Http) {
+    let clientId = "374191041786-rkapv68dh79mji0n7q0q95j9b54rngmg.apps.googleusercontent.com";
+    googlePlus
+      .login({
+        "scopes": "https://www.googleapis.com/auth/calendar.readonly",
+        "webClientId": clientId,
+        "offline": true
+      })
+      .then(login => {
+          let body = new URLSearchParams();
+          body.set("code", login.serverAuthCode);
+          body.set("client_id", clientId);
+          body.set("client_secret", "TEyKwBgPFmIx8rftqYRxj6Kb");
+          body.set("grant_type", "authorization_code");
+          http
+            .post("https://www.googleapis.com/oauth2/v4/token", body)
+            .subscribe(token => http
+              .get(
+                "https://www.googleapis.com/calendar/v3/calendars/sira20nselu48ng0e3udi9tpe8@group.calendar.google.com/events"
+                + "?singleEvents=true"
+                + "&orderBy=startTime"
+                + `&timeMin=${new Date().toISOString()}`,
+                {headers: new Headers({"Authorization": `Bearer ${token.json().access_token}`})}
+              )
+              .subscribe(events => console.log(this.events = events.json().items))
+            )
+        }
+      )
   }
 
 }
